89cf811b0214cf01a2ecc9a75f05169b61c236e9
VerakarnM <140242795+VerakarnM@users.noreply.github.com>
2026-05-19 02:54:15 +1000

    Add files via upload

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,4 +1,2 @@
 var vg_1 = "vis1.vg.json";
-vegaEmbed("#vis1", vg_1).then(function (result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#vis1", vg_1).then(function (result) {}).catch(console.error);
