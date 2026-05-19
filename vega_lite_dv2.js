132ae0aab7c218e3bc12a01c8ac79a17bbb8bbf7
VerakarnM <140242795+VerakarnM@users.noreply.github.com>
2026-05-19 13:04:18 +1000

    Add files via upload

--- vega_lite_dv2.js
+++ vega_lite_dv2.js
@@ -1,2 +1,5 @@
 var vg_1 = "vis1.vg.json";
 vegaEmbed("#vis1", vg_1).then(function (result) {}).catch(console.error);
+
+var vg_2 = "vis2.vg.json";
+vegaEmbed("#vis2", vg_2).then(function (result) {}).catch(console.error);
