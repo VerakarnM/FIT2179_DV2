2c1d084deee96ebb9abeacee9b48a7ce7432b216
VerakarnM <140242795+VerakarnM@users.noreply.github.com>
2026-06-04 22:22:08 +1000

    Add files via upload

--- vega_lite_dv2.js
+++ vega_lite_dv2.js
@@ -1,8 +1,29 @@
-var vg_1 = "vis1.vg.json";
-vegaEmbed("#vis1", vg_1).then(function (result) {}).catch(console.error);
+var vg_DualBarMigrate = "vis_bar_departure_arrival.vg.json";
+vegaEmbed("#visDualBarMigrate", vg_DualBarMigrate).then(function (result) {}).catch(console.error);
 
-var vg_2 = "vis2.vg.json";
-vegaEmbed("#vis2", vg_2).then(function (result) {}).catch(console.error);
+var vg_NetBar = "vis_bar_time_region_net.vg.json";
+vegaEmbed("#visNetBar", vg_NetBar).then(function (result) {}).catch(console.error);
 
-var vg_3 = "vis3.vg.json";
-vegaEmbed("#vis3", vg_3).then(function (result) {}).catch(console.error);
+var vg_DualBarGender = "vis_bar_time_sex.vg.json";
+vegaEmbed("#visDualBarGender", vg_DualBarGender).then(function (result) {}).catch(console.error);
+
+var vg_HeatArrival = "vis_heat_age_region_arrival.vg.json";
+vegaEmbed("#visHeatArrival", vg_HeatArrival).then(function (result) {}).catch(console.error);
+
+var vg_HeatDepart = "vis_heat_age_region_departure.vg.json";
+vegaEmbed("#visHeatDepart", vg_HeatDepart).then(function (result) {}).catch(console.error);
+
+var vg_LineArrival = "vis_line_time_region_arrival.vg.json";
+vegaEmbed("#visLineArrival", vg_LineArrival).then(function (result) {}).catch(console.error);
+
+var vg_LineDepart = "vis_line_time_region_departures.vg.json";
+vegaEmbed("#visLineDepart", vg_LineDepart).then(function (result) {}).catch(console.error);
+
+var vg_MapArrival = "vis_map_arrival.vg.json";
+vegaEmbed("#visMapArrival", vg_MapArrival).then(function (result) {}).catch(console.error);
+
+var vg_MapDepart = "vis_map_departure.vg.json";
+vegaEmbed("#visMapDepart", vg_MapDepart).then(function (result) {}).catch(console.error);
+
+var vg_NormalGender = "vis_normalized_sex.vg.json";
+vegaEmbed("#visNormalGender", vg_NormalGender).then(function (result) {}).catch(console.error);
